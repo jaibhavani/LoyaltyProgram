9de85c720612d6d7d8d84b7f7a5cafd71407a5b1
suhas sankolli <suhas sankolli>
2017-06-28 09:55:52 -0400

    Added Loyalty package code to Airline points chaincode

--- main/Airline/AirlinePoints.go
+++ main/Airline/AirlinePoints.go
@@ -3,10 +3,24 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"encoding/json"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/jaibhavani/LoyaltyProgram/LoyaltyPkgUtil"
-	
+		
 )
+type LoyaltyPointWallet struct {
+	Name         string `json:"name"`
+	Password     string `json:"password"`
+	PointBalance int    `json:"pointbalance"`
+}
+
+type PointsTransaction struct {
+	Name            string `json:"name"`
+	Entity          string `json:"entity"`
+	TransactionID   string `json:"transactionid"`
+	TransactionType string `json:"transactiontype"`
+	LoyaltyPoints   int    `json:"loyaltypoints"`
+}
 
 // SimpleChaincode example simple Chaincode implementation
 type SampleChaincode struct {
@@ -27,7 +41,7 @@ func addPoints(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) 
 		return nil, errors.New("Incorrect number of arguments. Expecting 5 walletid,entity name, transactionid, reward and points")
 	}
 
-	_, err := LoyaltyPkg.AddPointsToWallet(stub, args)
+	_, err := AddPointsToWallet(stub, args)
 
 	if err != nil {
 
@@ -38,6 +52,88 @@ func addPoints(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) 
 	logger.Info("Successfully Added points to user wallet " + args[0])
 
 	return nil, nil
+}
+
+
+func AddPointsToWallet(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	logger.Debug("Entering add points to Loyalty wallet ")
+
+	if len(args) < 5 {
+		return nil, errors.New("Incorrect number of arguments. Expecting  name, entity, transactionid, transactiontype, points")
+	}
+
+	var name = args[0]
+
+	wallet, err := stub.GetState(name)
+
+	if err != nil {
+		return nil, errors.New("Error while getting wallet data for user " + name)
+	}
+	// Check if user has wallet record in the ledger. If not, then create the wallet
+	if wallet == nil {
+		return nil, errors.New("No wallet data exists for user " + name)
+	}
+
+	// Store the reward transaction to the ledger
+
+	points, err := strconv.Atoi(args[4])
+
+	if err != nil {
+		return nil, errors.New("Expecting integer value for transaction points as arg 4")
+	}
+	var rewardTran PointsTransaction
+	rewardTran.Name = name
+	rewardTran.Entity = args[1]
+	rewardTran.TransactionID = args[2]
+	rewardTran.TransactionType = args[3]
+	rewardTran.LoyaltyPoints = points
+
+	rewardTranBytes, err := json.Marshal(rewardTran)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for reward transaction")
+	}
+
+	err = stub.PutState(args[0]+args[1]+args[2], rewardTranBytes)
+
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Debug("addd reward transaction to the ledger ")
+
+	var userWallet LoyaltyPointWallet
+	err = json.Unmarshal(wallet, &userWallet)
+
+	if err != nil {
+		return nil, errors.New("Failed to marshal string to struct of user " + name)
+	}
+
+	// Add the new points to the wallet balance
+
+	awardPoints, err := strconv.Atoi(args[3])
+
+	if err != nil {
+		return nil, errors.New("Points awarded from entity " + args[1] + "  must be integer")
+	}
+	userWallet.PointBalance = userWallet.PointBalance + awardPoints
+
+	userWalletByte, err := json.Marshal(userWallet)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string after updating points for user " + name)
+	}
+
+	err = stub.PutState(userWallet.Name, userWalletByte)
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Debug("Added points to wallet success fully ")
+
+	return nil, nil
+
 }
 
 // Init function
